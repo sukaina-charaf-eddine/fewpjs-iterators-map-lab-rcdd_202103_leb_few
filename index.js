1b156238272e642af86f6226ca8fafd4ab3c489f
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-01 13:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  });
 }
-
-
-
-const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
-   word => word[0].toUpperCase() + word.slice(1)
-).join(' ')
-
-)
