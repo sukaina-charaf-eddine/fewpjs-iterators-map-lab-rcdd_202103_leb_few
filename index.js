27371e84aaa63e8f42f582e8f1ebb2d0b1be6a09
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-05-01 13:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+
+
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+   word => word[0].toUpperCase() + word.slice(1)
+).join(' ')
+
+)
